b02bb84d946e2399b29c674dd5aba425fb818623
wangjiajen <wangmaggie228@gmail.com>
2023-09-12 14:52:41 +0800

    add df_to_dataset

--- bc5cdr_bert.py
+++ bc5cdr_bert.py
@@ -48,8 +48,38 @@ tag_name = ['O',
 # len(tag_name)=5 五個類別
 from datasets import Dataset, ClassLabel, Sequence, Features, Value, DatasetDict
 tags = ClassLabel(num_classes=len(tag_name), names=tag_name)
-# 建立字典 dataset_structure
+# 建立字典 dataset_structure：描述資料集的結構的一個定義
+# ner_tags是資料集中的一個特徵，而且它的類型是Sequence(序列資料)，且每個項目的類別是tags
 dataset_structure = {"ner_tags":Sequence(tags),
                  'tokens': Sequence(feature=Value(dtype='string'))}
 
+#In[]
+def df_to_dataset(df, columns=['ner_tags', 'tokens']):
+  assert set(['ner_tags', 'tokens']).issubset(df.columns)
 
+  ner_tags = df['ner_tags'].map(tags.str2int).values.tolist()
+  tokens = df['tokens'].values.tolist()
+  # 確保 tokens 和 ner_tags 中的每個元素都是列表
+  assert isinstance(tokens[0], list) 
+  assert isinstance(ner_tags[0], list)
+  dic = {'ner_tags':ner_tags, 'tokens':tokens}
+  # create dataset
+  dataset = Dataset.from_dict(mapping=dic,
+              features=Features(dataset_structure),)
+  return dataset
+  
+dataset = df_to_dataset(df) # 從train.txt變成df，然後轉成訓練資料dataset
+test_dataset =  df_to_dataset(test_df) # 從test-submit.txt變成test_df，然後轉成訓練資料test_dataset
+
+
+label_names = dataset["train"].features["ner_tags"].feature.names
+
+#In[]
+from transformers import AutoTokenizer
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased") 
+
+def tokenize_function(examples):
+    return tokenizer(examples["tokens"], padding="max_length",
+                     truncation=True, is_split_into_words=True)
+
+tokenized_datasets_ = dataset.map(tokenize_function, batched=True)
