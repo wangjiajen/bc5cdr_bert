8c5b8972dda4d6d47da5fe7438af0eeb630eae83
wangjiajen <wangmaggie228@gmail.com>
2023-09-29 10:09:04 +0800

    0.5

--- bc5cdr_bert.py
+++ bc5cdr_bert.py
@@ -22,12 +22,12 @@ model = AutoModelForTokenClassification.from_pretrained('bert-base-uncased', num
 dataset = load_dataset("tner/bc5cdr")
 train_data = dataset["train"]
 val_data = dataset["validation"]
-# df = pd.DataFrame(train_dataset)
+# df = pd.DataFrame(val_data)
 # print(df)
 # print(dataset)
 
 # Dataset
-class Dataset(Dataset):
+class CustomDataset(Dataset):
     def __init__(self, data, tokenizer):
         self.data = data
         self.tokenizer = tokenizer
@@ -36,46 +36,76 @@ class Dataset(Dataset):
         return len(self.data)
     
     def __getitem__(self, idx):
-        text = self.data[idx]["tokens"]
-        label = self.data[idx]["tags"]
+        texts = self.data[idx]["tokens"]
+        labels = self.data[idx]["tags"]
         
         # print(text)
         # print(label)
-        return text, label
+        return texts, labels
+categories = {
 
-def align_labels_with_tokens(labels, word_ids):
-    new_labels = []
-    current_word = None
-    # print(f"labels: {labels}, word_ids: {word_ids}")
-    # print(word_ids)
-    # print(len(word_ids))
-    # print(labels)
-    # print(len(labels))
-    for word_id in word_ids:
-        
-            if word_id != current_word:
-                # Start of a new word!
-                current_word = word_id
-                label = -100 if word_id is None else labels[word_id]
-                new_labels.append(label)
-            elif word_id is None:
-                # Special token
-                new_labels.append(-100)
+    0: 0,
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 4,
+
+# 0:'O',
+# 1:'B-Chemical',
+# 2:'B-Disease',
+# 3:'I-Disease',
+# 4:'I-Chemical',
+}
+def coffate_fn(examples):
+    texts, all_labels = [], []
+    for text, ner_labels in examples:
+        texts.append(text)
+        all_labels.append([categories[label] for label in ner_labels])
+    tokenized_inputs = tokenizer(texts,
+                                 truncation=True,
+                                 padding=True,
+                                
+                                 is_split_into_words=True,
+                                 max_length=128,
+                                 return_tensors="pt")
+    targets = []
+    for i, labels in enumerate(all_labels):
+        label_ids = []
+        for word_idx in tokenized_inputs.word_ids(batch_index=i):
+            # 将特殊符号的标签设置为-100，以便在计算损失函数时自动忽略
+            if word_idx is None:
+                label_ids.append(-100)
             else:
-                # Same word as previous token
-                label = -100
-                new_labels.append(label)      
-        # print(new_labels)
-
-
-    return new_labels
-
-
-
-
-
-
-
+                # 把标签设置到每个词的第一个token上
+                label_ids.append(labels[word_idx])
+        targets.append(label_ids)
+    labels_input = torch.tensor(targets)
+    return tokenized_inputs, labels_input
+# def align_labels_with_tokens(labels, word_ids):
+#     # print(f"labels: {labels}, word_ids: {word_ids}")
+#     new_labels = []
+#     current_word = None
+#     # print(f"labels: {labels}, word_ids: {word_ids}")
+#     # print(word_ids)
+#     # print(len(word_ids))
+#     # print(labels)
+#     # print(len(labels))
+#     for word_id in word_ids:
+        
+#             if word_id != current_word:
+#                 # Start of a new word!
+#                 current_word = word_id
+#                 label = -100 if word_id is None else labels[word_id]
+#                 new_labels.append(label)
+#             elif word_id is None:
+#                 # Special token
+#                 new_labels.append(-100)
+#             else:
+#                 # Same word as previous token
+#                 label = -100
+#                 new_labels.append(label)      
+#         # print(new_labels)
+#     return new_labels
 
 # def split_entity(label_sequence):
 #     entity_mark = dict()
@@ -119,30 +149,31 @@ def align_labels_with_tokens(labels, word_ids):
 
 #     return precision, recall, f1
 
-def collate_fn(batch):
-    texts, labels = zip(*batch)
+# def collate_fn(batch):
+#     texts, labels = zip(*batch)
 
-    # 標記文本
-    tokenized_inputs = tokenizer(
-        texts, truncation=True, is_split_into_words=True, padding=True, max_length=128
-    ,return_tensors="pt")
-    # print(texts)
-    # print(labels)
-    # print(tokenized_inputs.input_ids)
-    # print(tokenized_inputs.attention_mask)
-    # 處理標籤
-    new_labels = []
-    for i, label_list in enumerate(labels):
-        word_ids = tokenized_inputs.word_ids(i)
-        new_labels.append(align_labels_with_tokens(label_list, word_ids))
-    # print(new_labels)
+#     # 標記文本
+#     tokenized_inputs = tokenizer(
+#         texts, truncation=True, is_split_into_words=True, padding=True 
+#     ,return_tensors="pt")
+#     # ,max_length=128
+#     # print(texts)
+#     # print(labels)
+#     # print(tokenized_inputs.input_ids)
+#     # print(tokenized_inputs.attention_mask)
+#     # 處理標籤
+#     new_labels = []
+#     for i, label_list in enumerate(labels):
+#         word_ids = tokenized_inputs.word_ids(i)
+#         new_labels.append(align_labels_with_tokens(label_list, word_ids))
+#     # print(new_labels)
 
-    # 將輸入和標籤轉換為PyTorch張量
-    # input_ids = torch.tensor(tokenized_inputs.input_ids)
-    # attention_mask = torch.tensor(tokenized_inputs.attention_mask)
-    labels_input = torch.tensor(new_labels)
-    # print(labels_input)
-    return tokenized_inputs, labels_input
+#     # 將輸入和標籤轉換為PyTorch張量
+#     # input_ids = torch.tensor(tokenized_inputs.input_ids)
+#     # attention_mask = torch.tensor(tokenized_inputs.attention_mask)
+#     labels_input = torch.tensor(new_labels)
+#     # print(labels_input)
+#     return tokenized_inputs, labels_input
 
 # def tokenize_and_align_labels(examples):
 #     tokenized_inputs = tokenizer(
@@ -174,14 +205,14 @@ def collate_fn(batch):
 # print(tokenized_example)
 
 # 定義參數
-LR = 1e-6
+LR = 5e-5
 EPOCHS = 5 #總共要用全部的訓練樣本重複跑幾回合
 BATCH_SIZE = 8
 
-train_dataset = Dataset(train_data,tokenizer)
-train_loader = DataLoader(train_dataset, batch_size = BATCH_SIZE, shuffle = True, collate_fn=collate_fn)
-val_dataset = Dataset(val_data,tokenizer)
-val_loader = DataLoader(val_dataset, batch_size = BATCH_SIZE, shuffle = True, collate_fn=collate_fn)
+train_dataset = CustomDataset(train_data,tokenizer)
+train_loader = DataLoader(train_dataset, batch_size = BATCH_SIZE, shuffle = True, collate_fn=coffate_fn)
+val_dataset = CustomDataset(val_data,tokenizer)
+val_loader = DataLoader(val_dataset, batch_size = BATCH_SIZE, shuffle = True, collate_fn=coffate_fn)
 
 # Define the model, optimizer, and loss function
 model.to(device)
@@ -200,26 +231,27 @@ for epoch in range(EPOCHS):
         # attention_mask=a.attention_mask
         # optimizer.zero_grad()
         # outputs = model(input_ids=input_ids, attention_mask=attention_mask,labels=labels_input)
-        
-        inputs, labels_input = batch
-        inputs = {k: v.to(device) for k, v in inputs.items()}
-        labels = labels_input.to(device)
+        inputs, labels_input = [x.to(device) for x in batch]
+        # inputs, labels_input = batch
+        # inputs = {k: v.to(device) for k, v in inputs.items()}
+        # labels = labels_input.to(device)
         optimizer.zero_grad()
 
         outputs = model(**inputs)
         logits = outputs.logits
 
         # Reshape logits and labels to [batch_size * max_seq_length, num_labels]
-        logits = logits.view(-1, model.config.num_labels)
-        labels =labels.view(-1)
+        # logits = logits.view(-1, model.config.num_labels)
+        labels =labels_input.view(-1)
 
         # Compute the loss
-        loss = loss_fn(logits, labels)
+        loss = loss_fn(outputs.logits.view(-1, 5), labels)
         loss.backward()
         optimizer.step()
 
         total_loss += loss.item()
-
+        # print(f"inputs: {inputs}")
+        # print(f"labels_input: {labels_input}")
     average_loss = total_loss / len(train_loader)
     print(f'Epoch {epoch+1}, Loss: {average_loss:.4f}')
 
@@ -230,9 +262,11 @@ for epoch in range(EPOCHS):
 
     with torch.no_grad():
         for batch in tqdm(val_loader, desc=f'Validation Epoch {epoch+1}'):
-            inputs, labels_input = batch
-            inputs = {k: v.to(device) for k, v in inputs.items()}
-            labels = labels_input.to(device)
+            inputs, labels_input = [x.to(device) for x in batch]
+            labels = labels_input.view(-1)
+            # inputs, labels_input = batch
+            # inputs = {k: v.to(device) for k, v in inputs.items()}
+            # labels = labels_input.to(device)
 
             # data=[t.to(device) for t in batch]
             # a,labels_input=data[:]
@@ -245,19 +279,18 @@ for epoch in range(EPOCHS):
 
             # Reshape logits and labels to [batch_size * max_seq_length, num_labels]
             logits = logits.view(-1, model.config.num_labels)
-            labels = labels.view(-1)
+            
 
             _, predicted_labels = torch.max(logits, 1)
 
             val_true_labels.extend(labels.cpu().numpy())
-            val_pred_labels.extend(predicted_labels.cpu().numpy())
+            val_pred_labels.extend(predicted_labels.cpu().numpy())        
+            # print(f"inputs: {inputs}")
+            # print(f"labels_input: {labels_input}")
 
     # Calculate evaluation metrics
-    precision = precision_score(val_true_labels, val_pred_labels, average='macro')
+    precision = precision_score(val_true_labels, val_pred_labels, average='macro', zero_division=1)
     recall = recall_score(val_true_labels, val_pred_labels, average='macro')
     f1 = f1_score(val_true_labels, val_pred_labels, average='macro')
 
     print(f'Validation Precision: {precision:.4f}, Recall: {recall:.4f}, F1-score: {f1:.4f}')
-
-
-
